23219d1e88383cef5ea75d04f28b98adb896eca7
herjoh <73656823+herjoh@users.noreply.github.com>
2022-11-23 12:59:37 +0100

    Update CarsDAO.java

--- src/sample/modelV2/CarsDAO.java
+++ src/sample/modelV2/CarsDAO.java
@@ -3,7 +3,6 @@ package sample.modelV2;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import sample.model.Employee;
 import sample.util.DBUtil;
 
 import java.sql.ResultSet;
@@ -11,57 +10,34 @@ import java.sql.SQLException;
 
 public class CarsDAO {
 
-    //*******************************
-    //SELECT an Employee
-    //*******************************
-    public static Cars searchCar (String regNr) throws SQLException, ClassNotFoundException {
-        //Declare a SELECT statement
-        String selectStmt = "SELECT * FROM employees WHERE employee_id="+regNr;
 
-        //Execute SELECT statement
-        try {
-            //Get ResultSet from dbExecuteQuery method
-            ResultSet rsEmp = DBUtil.dbExecuteQuery(selectStmt);
-
-            //Send ResultSet to the getEmployeeFromResultSet method and get employee object
-            Employee car = getEmployeeFromResultSet(rsEmp);
-
-            //Return employee object
-            return null;
-        } catch (SQLException e) {
-            System.out.println("While searching an employee with " + regNr + " id, an error occurred: " + e);
-            //Return exception
-            throw e;
-        }
-    }
 
     //Use ResultSet from DB as parameter and set Employee Object's attributes and return employee object.
-    private static Employee getEmployeeFromResultSet(ResultSet rs) throws SQLException
+    private static Cars getCarFromResultSet(ResultSet rs) throws SQLException
     {
-        Employee emp = null;
+        Cars car = null;
         if (rs.next()) {
-            emp = new Employee();
-            emp.setEmployeeId(rs.getInt("EMPLOYEE_ID"));
-            emp.setFirstName(rs.getString("FIRST_NAME"));
-            emp.setLastName(rs.getString("LAST_NAME"));
-            emp.setEmail(rs.getString("EMAIL"));
-            emp.setPhoneNumber(rs.getString("PHONE_NUMBER"));
-            emp.setHireDate(rs.getDate("HIRE_DATE"));
-            emp.setJobId(rs.getString("JOB_ID"));
-            emp.setSalary(rs.getInt("SALARY"));
-            emp.setCommissionPct(rs.getDouble("COMMISSION_PCT"));
-            emp.setManagerId(rs.getInt("MANAGER_ID"));
-            emp.setDepartmantId(rs.getInt("DEPARTMENT_ID"));
+            car = new Cars();
+            car.setMerke(rs.getString("Merke"));
+            car.setModel(rs.getString("Modell"));
+            car.setEier(rs.getString("Eier"));
+            car.setÅrsmodel(rs.getInt("Årsmodel"));
+            car.setFarge(rs.getString("Farge"));
+            car.setOmråde(rs.getString("Område"));
+            car.setLedighet(rs.getBoolean("Ledighet"));
+            car.setDato(rs.getString("Dato"));
+            car.setRegestreringsNummer(rs.getString("Regnummer"));
+
         }
-        return emp;
+        return car;
     }
 
     //*******************************
     //SELECT Employees
     //*******************************
-    public static ObservableList<Employee> searchEmployees () throws SQLException, ClassNotFoundException {
+    public static ObservableList<Cars> searchEier () throws SQLException, ClassNotFoundException {
         //Declare a SELECT statement
-        String selectStmt = "SELECT * FROM employees";
+        String selectStmt = "SELECT * FROM Eier";
 
         //Execute SELECT statement
         try {
@@ -69,7 +45,7 @@ public class CarsDAO {
             ResultSet rsEmps = DBUtil.dbExecuteQuery(selectStmt);
 
             //Send ResultSet to the getEmployeeList method and get employee object
-            ObservableList<Employee> empList = getEmployeeList(rsEmps);
+            ObservableList<Cars> empList = getCarList(rsEmps);
 
             //Return employee object
             return empList;
@@ -81,84 +57,43 @@ public class CarsDAO {
     }
 
     //Select * from employees operation
-    private static ObservableList<Employee> getEmployeeList(ResultSet rs) throws SQLException, ClassNotFoundException {
+    private static ObservableList<Cars> getCarList(ResultSet rs) throws SQLException, ClassNotFoundException {
         //Declare a observable List which comprises of Employee objects
-        ObservableList<Employee> empList = FXCollections.observableArrayList();
+        ObservableList<Cars> empList = FXCollections.observableArrayList();
 
         while (rs.next()) {
-            Employee emp = new Employee();
-            emp.setEmployeeId(rs.getInt("EMPLOYEE_ID"));
-            emp.setFirstName(rs.getString("FIRST_NAME"));
-            emp.setLastName(rs.getString("LAST_NAME"));
-            emp.setEmail(rs.getString("EMAIL"));
-            emp.setPhoneNumber(rs.getString("PHONE_NUMBER"));
-            emp.setHireDate(rs.getDate("HIRE_DATE"));
-            emp.setJobId(rs.getString("JOB_ID"));
-            emp.setSalary(rs.getInt("SALARY"));
-            emp.setCommissionPct(rs.getDouble("COMMISSION_PCT"));
-            emp.setManagerId(rs.getInt("MANAGER_ID"));
-            emp.setDepartmantId(rs.getInt("DEPARTMENT_ID"));
+            Cars car = new Cars();
+            car.setMerke(rs.getString("Merke"));
+            car.setModel(rs.getString("Modell"));
+            car.setEier(rs.getString("Eier"));
+            car.setÅrsmodel(rs.getInt("Årsmodel"));
+            car.setFarge(rs.getString("Farge"));
+            car.setOmråde(rs.getString("Område"));
+            car.setLedighet(rs.getBoolean("Ledighet"));
+            car.setDato(rs.getString("Dato"));
+            car.setRegestreringsNummer(rs.getString("Regnummer"));
+
             //Add employee to the ObservableList
-            empList.add(emp);
+            empList.add(car);
         }
         //return empList (ObservableList of Employees)
         return empList;
     }
 
-    //*************************************
-    //UPDATE an employee's email address
-    //*************************************
-    public static void updateEmpEmail (String empId, String empEmail) throws SQLException, ClassNotFoundException {
-        //Declare a UPDATE statement
-        String updateStmt =
-                "BEGIN\n" +
-                        "   UPDATE employees\n" +
-                        "      SET EMAIL = '" + empEmail + "'\n" +
-                        "    WHERE EMPLOYEE_ID = " + empId + ";\n" +
-                        "   COMMIT;\n" +
-                        "END;";
-
-        //Execute UPDATE operation
-        try {
-            DBUtil.dbExecuteUpdate(updateStmt);
-        } catch (SQLException e) {
-            System.out.print("Error occurred while UPDATE Operation: " + e);
-            throw e;
-        }
-    }
-
-    //*************************************
-    //DELETE an employee
-    //*************************************
-    public static void deleteEmpWithId (String empId) throws SQLException, ClassNotFoundException {
-        //Declare a DELETE statement
-        String updateStmt =
-                "BEGIN\n" +
-                        "   DELETE FROM employees\n" +
-                        "         WHERE employee_id ="+ empId +";\n" +
-                        "   COMMIT;\n" +
-                        "END;";
-
-        //Execute UPDATE operation
-        try {
-            DBUtil.dbExecuteUpdate(updateStmt);
-        } catch (SQLException e) {
-            System.out.print("Error occurred while DELETE Operation: " + e);
-            throw e;
-        }
-    }
 
     //*************************************
     //INSERT an employee
     //*************************************
-    public static void insertEmp (String name, String lastname, String email) throws SQLException, ClassNotFoundException {
+    public static void insertCar (String merke, String model, String eier, Integer årsmodel, String farge, String område,
+                                  int ledighet, String dato, String regestreringsNummer ) throws SQLException, ClassNotFoundException {
         //Declare a DELETE statement
         String updateStmt =
                 "BEGIN\n" +
-                        "INSERT INTO employees\n" +
-                        "(EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, HIRE_DATE, JOB_ID)\n" +
+                        "INSERT INTO karsdb\n" +
+                        "(Merke, Model, Eier, Årsmodel, Farge, Område, Ledighet, Dato, Registreringsnummer)\n" +
                         "VALUES\n" +
-                        "(sequence_employee.nextval, '"+name+"', '"+lastname+"','"+email+"', SYSDATE, 'IT_PROG');\n" +
+                        "(sequence_employee.nextval, '"+merke+"', '"+model+"','"+eier+"','"+årsmodel+"'," +
+                        " '"+farge+"','"+område+"','"+ledighet+"', '"+dato+"','"+regestreringsNummer+"' );\n" +
                         "END;";
 
         //Execute DELETE operation
